09b8c2cd99c6b4a6b986df6aaa6592c0c4190df7
Chikoti Bhuvana Sri <chbhuvana0505@gmail.com>
2025-11-03 19:22:47 +0530

    Add files via upload

---
+++ Day1_selectbasics.sql
@@ -0,0 +1,12 @@
+/*Retrieve all columns from the patients table.*/
+SELECT * FROM patients;
+
+/*Select only the patient_id, name, and age columns from the patients table.*/
+SELECT patient_id,name,age FROM patients;
+
+/*Display the first 10 records from the services_weekly table.*/
+SELECT * FROM services_weekly LIMIT 10;
+
+
+/*DAILY CHALLENGE DAY 1:List all unique hospital services available in the hospital.*/
+SELECT DISTINCT service as unique_service FROM services_weekly ;
